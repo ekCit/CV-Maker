a60b260d9d8cecbbd42b30002267f9d4e9b361cf
ekCit <crmrc2014@hotmail.com>
2016-08-22 10:40:32 +0800

    commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,19 @@
 function generatePdf(){
     
     var printdoc = new jsPDF();
-    printdoc.fromHTML($("#cv").get(0), 10, 10, {'width':180});
+    printdoc.fromHTML($("#cv").get(0), 10, 10, {'width':180},    function(dispose) {
+        // dispose: object with X, Y of the last line add to the PDF
+        //          this allow the insertion of new lines after html
+        // pdf.save('Test.pdf');
+
+        if (navigator.msSaveBlob) {
+            var string = printdoc.output('datauristring');
+        } else {
+            var string = printdoc.output('bloburi');
+        }
+
+        $('.previewIFRAME').attr('src', string);
+    });
     printdoc.output("dataurlnewwindow"); 
 }
 
